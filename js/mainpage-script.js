9675db1d02da08092312ae1bfb33b823201a4f3f
Carol Xu <carolx715@gmail.com>
2020-05-17 17:54:34 -0400

    trying out js animations on website

--- js/mainpage-script.js
+++ js/mainpage-script.js
@@ -1,9 +1,20 @@
 // making circles move up and down
 // probably using a sin curve to determine position? At different time intervals?
 
-function moveCircleUp() {
-	
-}
-//function bobbingCircles() {
-//	
+var c = document.getElementsByClassName("circle");
+
+//function moveCircleUp() {
+//	var cNum = 0; // something to do with getting element based on hover
+//	c[i].style.top = 
 //}
+
+var start = 0.0;
+
+function bobbingCircles() {
+	for (i=0; i<c.length; i++) {
+		c[i].style.top = (10 * Math.sin(start-i) - 10) + "px";
+	}
+	start += 0.01;
+}
+
+window.setInterval(bobbingCircles, 5);
