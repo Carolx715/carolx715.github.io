fd0783bc2d4fe5c1d299fef2dff901a02c84b222
Carol Xu <carolx715@gmail.com>
2020-05-17 15:54:28 -0400

    implementing cross device compatibility

---
+++ js/mainpage-script.js
@@ -0,0 +1,9 @@
+// making circles move up and down
+// probably using a sin curve to determine position? At different time intervals?
+
+function moveCircleUp() {
+	
+}
+//function bobbingCircles() {
+//	
+//}
