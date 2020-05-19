0422bd58dd8231e930c6ced3754c0d255fe11198
Carol Xu <carolx715@gmail.com>
2020-05-19 14:24:45 -0400

    updated js to make circle bounce

--- js/mainpage-script.js
+++ js/mainpage-script.js
@@ -2,19 +2,49 @@
 // probably using a sin curve to determine position? At different time intervals?
 
 var c = document.getElementsByClassName("circle");
-
-//function moveCircleUp() {
-//	var cNum = 0; // something to do with getting element based on hover
-//	c[i].style.top = 
-//}
-
+var cNum = 0;
+var originalTop;
+var interval;
 var start = 0.0;
 
-function bobbingCircles() {
-	for (i=0; i<c.length; i++) {
-		c[i].style.top = (0.3 * Math.sin(start-i)) + "em";
-	}
-	start += 0.01;
+function onHoverOver(i) {
+	originalTop = c[i].style.top;
+	cNum = i;
+	interval = window.setInterval(bobbingCircle, 5);
+	//	c[cNum].style.top = 0.3*Math.sin(Math.PI) + "em";
+	//	if (!c[cNum].mouseout) {
+	//		return;
+	//	}
 }
 
-window.setInterval(bobbingCircles, 5);
+function stopHoverOver(i) {
+	var dest = originalTop;
+	c[i].style.top = dest;
+	//	while (-0.4*Math.sin(start) <= 0) {
+	//		interval = window.setInterval(bobbingCircle, 5);
+	//	}
+
+	clearInterval(interval);
+	start = 0;
+}
+
+function stahp() {
+	if (-0.4 * Math.sin(start) <= 0.1 && -0.4 * Math.sin(start) >= -0.1) {
+		clearInterval(interval);
+	}
+}
+
+//function bobbingCircles() {
+//	for (i=0; i<c.length; i++) {
+//		c[i].style.top = (0.3 * Math.sin(start-i)) + "em";
+//	}
+//	start += 0.01;
+//}
+
+function bobbingCircle() {
+	c[cNum].style.top = (-0.5 * Math.abs(Math.sin(start)) + 0.85) + "em";
+	//	c[cNum].style.top = -200 + "px";
+	start += 0.02;
+}
+
+//window.setInterval(bobbingCircles, 5);
